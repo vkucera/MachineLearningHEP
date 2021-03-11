7aae14f4636321ac34564f718da5537075f161c1
Vít Kučera <vit.kucera@cern.ch>
2021-03-11 01:01:17 +0100

    Apply clang-format.

--- Analyses/ALICE_D2H_vs_mult_pp13/AliHFSystErrToYaml.C
+++ Analyses/ALICE_D2H_vs_mult_pp13/AliHFSystErrToYaml.C
@@ -22,46 +22,45 @@ using std::endl;
 #include "TSystem.h"
 #include "AliHFSystErr.h"
 
+void AliHFSystErrToYaml(TString inputrootfile, TString outputyamlfile, Bool_t isLc = kFALSE)
+{
 
-void AliHFSystErrToYaml(TString inputrootfile, TString outputyamlfile, Bool_t isLc = kFALSE){
-  
   /*
   const Int_t nPtBins = 5;
   Float_t PtBins[nPtBins+1] = {2., 4., 6., 8., 12., 24.};
   Float_t midPtBins[nPtBins] = {3., 5., 7., 10., 18.};
   */
   const Int_t nPtBins = 6;
-  Float_t PtBins[nPtBins+1] = {1., 2., 4., 6., 8., 12., 24.};
+  Float_t PtBins[nPtBins + 1] = {1., 2., 4., 6., 8., 12., 24.};
   Float_t midPtBins[nPtBins] = {1.5, 2.9, 5., 7., 10., 18.};
-  
-  TFile* fInput =  new TFile(inputrootfile.Data(),"read");
-  
-  AliHFSystErr* oSystErr = (AliHFSystErr*)fInput->Get("AliHFSystErr");
-  
-  ofstream fOutYaml (outputyamlfile.Data());
-  
-  fOutYaml <<endl <<endl;
-  fOutYaml << "#Extracted automatically from AliHFSystErrObject of file: " << inputrootfile << endl;
-  
-  // yield
-  fOutYaml <<"names: [\"yield\", \"cut\", \"track\", \"ptshape\", \"feeddown_mult\", \"trigger\", \"multiplicity_weights\", \"multiplicity_interval\", \"branching_ratio\", \"sigmav0\"]" <<endl <<endl;
-  
-  
-  fOutYaml <<"yield:" <<endl;
-  for(int ipt=0; ipt<nPtBins; ipt++){
-    fOutYaml <<"    - [0., 0., " <<oSystErr->GetRawYieldErr(midPtBins[ipt]) <<", " <<oSystErr->GetRawYieldErr(midPtBins[ipt]) <<"]" <<endl;
-  }
-  fOutYaml <<endl;
-  
-  
-  fOutYaml <<"cut:" <<endl;
-  for(int ipt=0; ipt<nPtBins; ipt++){
-    //fOutYaml <<"    - [0., 0., " <<oSystErr->GetCutsEffErr(midPtBins[ipt]) <<", " <<oSystErr->GetCutsEffErr(midPtBins[ipt]) <<"]" <<endl;
-    fOutYaml <<"    - [0., 0., " << TMath::Sqrt( oSystErr->GetCutsEffErr(midPtBins[ipt]) * oSystErr->GetCutsEffErr(midPtBins[ipt]) + oSystErr->GetPIDEffErr(midPtBins[ipt]) * oSystErr->GetPIDEffErr(midPtBins[ipt])) <<", " << TMath::Sqrt( oSystErr->GetCutsEffErr(midPtBins[ipt]) * oSystErr->GetCutsEffErr(midPtBins[ipt]) + oSystErr->GetPIDEffErr(midPtBins[ipt]) * oSystErr->GetPIDEffErr(midPtBins[ipt]))  <<"]" <<endl;
-  }
-  fOutYaml <<endl;
 
-  
+  TFile* fInput = new TFile(inputrootfile.Data(), "read");
+
+  AliHFSystErr* oSystErr = (AliHFSystErr*)fInput->Get("AliHFSystErr");
+
+  ofstream fOutYaml(outputyamlfile.Data());
+
+  fOutYaml << endl
+           << endl;
+  fOutYaml << "#Extracted automatically from AliHFSystErrObject of file: " << inputrootfile << endl;
+
+  // yield
+  fOutYaml << "names: [\"yield\", \"cut\", \"track\", \"ptshape\", \"feeddown_mult\", \"trigger\", \"multiplicity_weights\", \"multiplicity_interval\", \"branching_ratio\", \"sigmav0\"]" << endl
+           << endl;
+
+  fOutYaml << "yield:" << endl;
+  for (int ipt = 0; ipt < nPtBins; ipt++) {
+    fOutYaml << "    - [0., 0., " << oSystErr->GetRawYieldErr(midPtBins[ipt]) << ", " << oSystErr->GetRawYieldErr(midPtBins[ipt]) << "]" << endl;
+  }
+  fOutYaml << endl;
+
+  fOutYaml << "cut:" << endl;
+  for (int ipt = 0; ipt < nPtBins; ipt++) {
+    //fOutYaml <<"    - [0., 0., " <<oSystErr->GetCutsEffErr(midPtBins[ipt]) <<", " <<oSystErr->GetCutsEffErr(midPtBins[ipt]) <<"]" <<endl;
+    fOutYaml << "    - [0., 0., " << TMath::Sqrt(oSystErr->GetCutsEffErr(midPtBins[ipt]) * oSystErr->GetCutsEffErr(midPtBins[ipt]) + oSystErr->GetPIDEffErr(midPtBins[ipt]) * oSystErr->GetPIDEffErr(midPtBins[ipt])) << ", " << TMath::Sqrt(oSystErr->GetCutsEffErr(midPtBins[ipt]) * oSystErr->GetCutsEffErr(midPtBins[ipt]) + oSystErr->GetPIDEffErr(midPtBins[ipt]) * oSystErr->GetPIDEffErr(midPtBins[ipt])) << "]" << endl;
+  }
+  fOutYaml << endl;
+
   /*
   // PID  ---> Included in cut as for ML!
   fOutYaml <<"PID:" <<endl;
@@ -70,75 +69,68 @@ void AliHFSystErrToYaml(TString inputrootfile, TString outputyamlfile, Bool_t is
   }
   fOutYaml <<endl;
   */
-  
 
-  fOutYaml <<"track:" <<endl;
-  for(int ipt=0; ipt<nPtBins; ipt++){
-    fOutYaml <<"    - [0., 0., " <<oSystErr->GetTrackingEffErr(midPtBins[ipt]) <<", " <<oSystErr->GetTrackingEffErr(midPtBins[ipt]) <<"]" <<endl;
+  fOutYaml << "track:" << endl;
+  for (int ipt = 0; ipt < nPtBins; ipt++) {
+    fOutYaml << "    - [0., 0., " << oSystErr->GetTrackingEffErr(midPtBins[ipt]) << ", " << oSystErr->GetTrackingEffErr(midPtBins[ipt]) << "]" << endl;
   }
-  fOutYaml <<endl;
-  
+  fOutYaml << endl;
 
-  fOutYaml <<"ptshape:" <<endl;
-  for(int ipt=0; ipt<nPtBins; ipt++){
-    fOutYaml <<"    - [0., 0., " <<oSystErr->GetMCPtShapeErr(midPtBins[ipt]) <<", " <<oSystErr->GetMCPtShapeErr(midPtBins[ipt]) <<"]" <<endl;
+  fOutYaml << "ptshape:" << endl;
+  for (int ipt = 0; ipt < nPtBins; ipt++) {
+    fOutYaml << "    - [0., 0., " << oSystErr->GetMCPtShapeErr(midPtBins[ipt]) << ", " << oSystErr->GetMCPtShapeErr(midPtBins[ipt]) << "]" << endl;
   }
-  fOutYaml <<endl;
-  
-  
-  fOutYaml <<"feeddown_mult:" <<endl;
+  fOutYaml << endl;
+
+  fOutYaml << "feeddown_mult:" << endl;
   //TString fpromptname = "gFcConservative";
   //if(isLc) fpromptname = "gFcCorrConservative";
   //TGraphAsymmErrors* fPrompt = (TGraphAsymmErrors*)fInput->Get(fpromptname.Data());
-  for(int ipt=0; ipt<nPtBins; ipt++){
+  for (int ipt = 0; ipt < nPtBins; ipt++) {
     //double errYhigh = fPrompt->GetErrorYhigh(ipt+1);
     //double errYlow = fPrompt->GetErrorYlow(ipt+1);
     //NOT PART OF ALIHFSYSTERR, TO BE FILLED IN BY HAND
-    fOutYaml <<"    - [0., 0., " <<0. <<", " <<0. <<"]" <<endl;
+    fOutYaml << "    - [0., 0., " << 0. << ", " << 0. << "]" << endl;
   }
-  fOutYaml <<endl;
-  
+  fOutYaml << endl;
 
-  fOutYaml <<"trigger:" <<endl;
-  for(int ipt=0; ipt<nPtBins; ipt++){
+  fOutYaml << "trigger:" << endl;
+  for (int ipt = 0; ipt < nPtBins; ipt++) {
     //NOT PART OF ALIHFSYSTERR, TO BE FILLED IN BY HAND
-    fOutYaml <<"    - [0., 0., " <<0. <<", " <<0. <<"]" <<endl;
+    fOutYaml << "    - [0., 0., " << 0. << ", " << 0. << "]" << endl;
   }
-  fOutYaml <<endl;
-  
+  fOutYaml << endl;
 
-  fOutYaml <<"multiplicity_weights:" <<endl;
-  for(int ipt=0; ipt<nPtBins; ipt++){
+  fOutYaml << "multiplicity_weights:" << endl;
+  for (int ipt = 0; ipt < nPtBins; ipt++) {
     //NOT PART OF ALIHFSYSTERR, TO BE FILLED IN BY HAND
-    fOutYaml <<"    - [0., 0., " <<0. <<", " <<0. <<"]" <<endl;
+    fOutYaml << "    - [0., 0., " << 0. << ", " << 0. << "]" << endl;
   }
-  fOutYaml <<endl;
-  
-  
-  fOutYaml <<"multiplicity_interval:" <<endl;
-  for(int ipt=0; ipt<nPtBins; ipt++){
+  fOutYaml << endl;
+
+  fOutYaml << "multiplicity_interval:" << endl;
+  for (int ipt = 0; ipt < nPtBins; ipt++) {
     //NOT PART OF ALIHFSYSTERR, TO BE FILLED IN BY HAND
-    fOutYaml <<"    - [0., 0., " <<0. <<", " <<0. <<"]" <<endl;
+    fOutYaml << "    - [0., 0., " << 0. << ", " << 0. << "]" << endl;
   }
-  fOutYaml <<endl;
-  
-  
-  fOutYaml <<"branching_ratio:" <<endl;
-  for(int ipt=0; ipt<nPtBins; ipt++){
-    if(!isLc) fOutYaml <<"    - [0., 0., " <<0.0129 <<", " <<0.0129 <<"]" <<endl;
-    else fOutYaml <<"    - [0., 0., " <<0.053 <<", " <<0.053 <<"]" <<endl;
+  fOutYaml << endl;
+
+  fOutYaml << "branching_ratio:" << endl;
+  for (int ipt = 0; ipt < nPtBins; ipt++) {
+    if (!isLc)
+      fOutYaml << "    - [0., 0., " << 0.0129 << ", " << 0.0129 << "]" << endl;
+    else
+      fOutYaml << "    - [0., 0., " << 0.053 << ", " << 0.053 << "]" << endl;
     //fOutYaml <<"    - [0., 0., " <<oSystErr->GetBRErr() <<", " <<oSystErr->GetBRErr() <<"]" <<endl;
   }
-  fOutYaml <<endl;
-  
-  
-  fOutYaml <<"sigmav0:" <<endl;
-  for(int ipt=0; ipt<nPtBins; ipt++){
-    fOutYaml <<"    - [0., 0., " <<0.05 <<", " <<0.05 <<"]" <<endl;
+  fOutYaml << endl;
+
+  fOutYaml << "sigmav0:" << endl;
+  for (int ipt = 0; ipt < nPtBins; ipt++) {
+    fOutYaml << "    - [0., 0., " << 0.05 << ", " << 0.05 << "]" << endl;
     //fOutYaml <<"    - [0., 0., " <<oSystErr->GetNormErr() <<", " <<oSystErr->GetNormErr() <<"]" <<endl;
   }
-  fOutYaml <<endl;
-  
-  
+  fOutYaml << endl;
+
   fOutYaml.close();
 }
--- cplusutilities/merge_and_fit_invmasshisto.C
+++ cplusutilities/merge_and_fit_invmasshisto.C
@@ -11,161 +11,159 @@
 
 #include "AliHFInvMassFitter.h"
 
-
 Double_t mass = 2.286;
-Double_t min_fit = mass-0.14;
-Double_t max_fit = mass+0.14;
-Int_t signal_fit_f = 0;    // kGaus=0, k2Gaus=1, k2GausSigmaRatioPar=2
-Int_t background_fit_f = 2;// kExpo=0, kLin=1, kPol2=2, kNoBk=3, kPow=4, kPowEx=5
+Double_t min_fit = mass - 0.14;
+Double_t max_fit = mass + 0.14;
+Int_t signal_fit_f = 0;     // kGaus=0, k2Gaus=1, k2GausSigmaRatioPar=2
+Int_t background_fit_f = 2; // kExpo=0, kLin=1, kPol2=2, kNoBk=3, kPow=4, kPowEx=5
 Int_t rebin = 4;
 
 const int nptbins = 1;
-float ptlimits[nptbins+1] = {6, 8};
+float ptlimits[nptbins + 1] = {6, 8};
 TString dir[nptbins] = {"/home/ginnocen/MachineLearningHEP/machine_learning_hep/datapklanalysisLHC18q"};
 
 const int nprob_test = 1;
-TString   prob_test[nprob_test] = {"0.70"};
+TString prob_test[nprob_test] = {"0.70"};
 
 const int nfiles = 1;
-TString   files[nfiles] = {"LHC18q"};
-TString   cent = "3050";
+TString files[nfiles] = {"LHC18q"};
+TString cent = "3050";
 
 bool fixsigma = true;
 float sigmafix[nptbins] = {0.013};
 
-void merge_and_fit_invmasshisto(){
-    
-    TFile *fout = new TFile(Form("raw_yields_%s.root",cent.Data()),"RECREATE");
-    TH1F* h_raw_signal[nprob_test];
-    for(int k=0; k<nprob_test; k++){
-        h_raw_signal[k] = new TH1F(Form("h_raw_signal_prob%s",prob_test[k].Data()),";#it{p}_{T} (GeV/#it{c});raw yield",nptbins,ptlimits);
-    }
-        
-    
-    for(int i=0; i<nptbins; i++){
-        TCanvas *c_fit = new TCanvas(Form("fits_%.0f_%.0f",ptlimits[i],ptlimits[i+1]),Form("fits_%.0f_%.0f",ptlimits[i],ptlimits[i+1]));
-        c_fit->Divide(nprob_test,2);
-        
-        for(int iprob=0; iprob<nprob_test; iprob++){
-            TH1F *hmerge=0x0;
-            TCanvas *c_fit_prob = new TCanvas(Form("fits_prob%s_%.0f_%.0f",prob_test[iprob].Data(),ptlimits[i],ptlimits[i+1]),Form("fits_prob%s_%.0f_%.0f",prob_test[iprob].Data(),ptlimits[i],ptlimits[i+1]));
-            c_fit_prob->Divide(3,2);
-            // single period analysis
-            for(int ifil=0; ifil<nfiles; ifil++){
-		    std::cout<<Form("%s/masshistoLctopK0sPbPbCen%s%s_%.0f_%.0f.root",dir[i].Data(),cent.Data(),"_byHand",ptlimits[i], ptlimits[i+1])<<std::endl;
-		TFile *f = TFile::Open(Form("%s/masshisto%.0f_%.0f_%s.root",dir[i].Data(),ptlimits[i], ptlimits[i+1],prob_test[iprob].Data()));
-                
-		TH1F *h = (TH1F*)f->Get("hmass");
-                h->SetName(Form("h_invmass%s_%.0f_%.0f_prob%s",files[ifil].Data(),ptlimits[i],ptlimits[i+1],prob_test[iprob].Data()));
-                if(ifil==0){
-                    hmerge=(TH1F*)h->Clone("h_merge");
-                    hmerge->SetName(Form("h_invmasstot_%.0f_%.0f_prob%s",ptlimits[i],ptlimits[i+1],prob_test[iprob].Data()));
-                }
-                else{
-                    hmerge->Add(h);
-                }
-                c_fit_prob->cd(ifil+1);
-                gPad->SetTicks();
-                h->Rebin(rebin);
-                Float_t bin_width = h->GetXaxis()->GetBinWidth(3);
-                TString histo_title=Form("%.0f-%.0f prob>%s %s; #it{M} (GeV/#it{c}^{2}); Counts/%.0f MeV/#it{c}^{2}",ptlimits[i],ptlimits[i+1],prob_test[iprob].Data(),files[ifil].Data(),bin_width*1000.);
-                h->SetTitle(histo_title.Data());
-                AliHFInvMassFitter *fitter = new AliHFInvMassFitter(h,min_fit,max_fit,background_fit_f,signal_fit_f);
-                fitter->SetUseLikelihoodFit();
-                fitter->SetInitialGaussianMean(mass);
-                if(fixsigma)fitter->SetFixGaussianSigma(sigmafix[i]);
-                Bool_t out=fitter->MassFitter(0);
-                if(!out) {
-                    fitter->GetHistoClone()->Draw();
-                }
-                fitter->DrawHere(gPad);
-                Double_t sigma=fitter->GetSigma();
-                fout->cd();
-                h->Write();
-                
-                // subtracting background
-                TF1 *bkgf = (TF1*)fitter->GetBackgroundFullRangeFunc();
-                TH1F *hbkg = (TH1F*)h->Clone("hbkg");
-                TH1F *hsigsub = (TH1F*)h->Clone("hsigsub");
-                for(int j=0; j<h->GetNbinsX(); j++){
-                    float bkg=bkgf->Eval(hbkg->GetBinCenter(j+1));
-                    float bkge=TMath::Sqrt(bkg);
-                    hbkg->SetBinContent(j+1,bkg);
-                    hbkg->SetBinError(j+1,bkge);
-                }
-                //hbkg->Draw("same");
-                hsigsub->Add(hbkg,-1.);
-                c_fit_prob->cd(ifil+4);
-                AliHFInvMassFitter *fitterbkg = new AliHFInvMassFitter(hsigsub,min_fit,max_fit,3,signal_fit_f);
-                fitterbkg->SetUseChi2Fit();
-                fitterbkg->SetInitialGaussianMean(mass);
-                fitterbkg->SetFixGaussianSigma(sigma);
-                Bool_t out2=fitterbkg->MassFitter(0);
-                if(!out2) {
-                    fitterbkg->GetHistoClone()->Draw();
-                }
-                fitterbkg->DrawHere(gPad);
-            }
-            // merging 2 periods
-            hmerge->Rebin(rebin);
-            Float_t bin_width = hmerge->GetXaxis()->GetBinWidth(3);
-            TString histo_title=Form("%.0f-%.0f prob>%s; #it{M} (GeV/#it{c}^{2}); Counts/%.0f MeV/#it{c}^{2}",ptlimits[i],ptlimits[i+1],prob_test[iprob].Data(),bin_width*1000.);
-            hmerge->SetTitle(histo_title.Data());
-            c_fit->cd(iprob+1);
-            gPad->SetTicks();
-            AliHFInvMassFitter *fitter = new AliHFInvMassFitter(hmerge,min_fit,max_fit,background_fit_f,signal_fit_f);
-            fitter->SetUseLikelihoodFit();
-            fitter->SetInitialGaussianMean(mass);
-            if(fixsigma)fitter->SetFixGaussianSigma(sigmafix[i]);
-            Bool_t out=fitter->MassFitter(0);
-            if(!out) {
-                fitter->GetHistoClone()->Draw();
-            }
-            fitter->DrawHere(gPad);
-            c_fit_prob->cd(3);
-            fitter->DrawHere(gPad);
-            Double_t sigma=fitter->GetSigma();
-            double rawyield = fitter->GetRawYield();
-            double rawyielderr = fitter->GetRawYieldError();
-            h_raw_signal[iprob]->SetBinContent(i+1,rawyield);
-            h_raw_signal[iprob]->SetBinError(i+1,rawyielderr);
-            
-            //h_signal
-            
-            fout->cd();
-            hmerge->Write();
-            
-            // subtracting background
-            TF1 *bkgf = (TF1*)fitter->GetBackgroundFullRangeFunc();
-            TH1F *hbkg = (TH1F*)hmerge->Clone("hbkg");
-            TH1F *hsigsub = (TH1F*)hmerge->Clone("hsigsub");
-            for(int j=0; j<hmerge->GetNbinsX(); j++){
-                float bkg=bkgf->Eval(hbkg->GetBinCenter(j+1));
-                float bkge=TMath::Sqrt(bkg);
-                hbkg->SetBinContent(j+1,bkg);
-                hbkg->SetBinError(j+1,bkge);
-            }
-            hsigsub->Add(hbkg,-1.);
-            c_fit_prob->cd(6);
-            AliHFInvMassFitter *fitterbkg = new AliHFInvMassFitter(hsigsub,min_fit,max_fit,3,signal_fit_f);
-            //fitterbkg->SetUseLikelihoodFit();
-            fitterbkg->SetUseChi2Fit();
-            fitterbkg->SetInitialGaussianMean(mass);
-            fitterbkg->SetFixGaussianSigma(sigma);
-            Bool_t out2=fitterbkg->MassFitter(0);
-            if(!out2) {
-                fitterbkg->GetHistoClone()->Draw();
-            }
-            fitterbkg->DrawHere(gPad);
-            c_fit->cd(iprob+4);
-            fitterbkg->DrawHere(gPad);
-            c_fit_prob->Write();
-	}
-    }
-    fout->cd();
-    for(int k=0; k<nprob_test; k++){
-        h_raw_signal[k]->Write();
-    	
-    }
-}
+void merge_and_fit_invmasshisto()
+{
 
+  TFile* fout = new TFile(Form("raw_yields_%s.root", cent.Data()), "RECREATE");
+  TH1F* h_raw_signal[nprob_test];
+  for (int k = 0; k < nprob_test; k++) {
+    h_raw_signal[k] = new TH1F(Form("h_raw_signal_prob%s", prob_test[k].Data()), ";#it{p}_{T} (GeV/#it{c});raw yield", nptbins, ptlimits);
+  }
+
+  for (int i = 0; i < nptbins; i++) {
+    TCanvas* c_fit = new TCanvas(Form("fits_%.0f_%.0f", ptlimits[i], ptlimits[i + 1]), Form("fits_%.0f_%.0f", ptlimits[i], ptlimits[i + 1]));
+    c_fit->Divide(nprob_test, 2);
+
+    for (int iprob = 0; iprob < nprob_test; iprob++) {
+      TH1F* hmerge = 0x0;
+      TCanvas* c_fit_prob = new TCanvas(Form("fits_prob%s_%.0f_%.0f", prob_test[iprob].Data(), ptlimits[i], ptlimits[i + 1]), Form("fits_prob%s_%.0f_%.0f", prob_test[iprob].Data(), ptlimits[i], ptlimits[i + 1]));
+      c_fit_prob->Divide(3, 2);
+      // single period analysis
+      for (int ifil = 0; ifil < nfiles; ifil++) {
+        std::cout << Form("%s/masshistoLctopK0sPbPbCen%s%s_%.0f_%.0f.root", dir[i].Data(), cent.Data(), "_byHand", ptlimits[i], ptlimits[i + 1]) << std::endl;
+        TFile* f = TFile::Open(Form("%s/masshisto%.0f_%.0f_%s.root", dir[i].Data(), ptlimits[i], ptlimits[i + 1], prob_test[iprob].Data()));
+
+        TH1F* h = (TH1F*)f->Get("hmass");
+        h->SetName(Form("h_invmass%s_%.0f_%.0f_prob%s", files[ifil].Data(), ptlimits[i], ptlimits[i + 1], prob_test[iprob].Data()));
+        if (ifil == 0) {
+          hmerge = (TH1F*)h->Clone("h_merge");
+          hmerge->SetName(Form("h_invmasstot_%.0f_%.0f_prob%s", ptlimits[i], ptlimits[i + 1], prob_test[iprob].Data()));
+        } else {
+          hmerge->Add(h);
+        }
+        c_fit_prob->cd(ifil + 1);
+        gPad->SetTicks();
+        h->Rebin(rebin);
+        Float_t bin_width = h->GetXaxis()->GetBinWidth(3);
+        TString histo_title = Form("%.0f-%.0f prob>%s %s; #it{M} (GeV/#it{c}^{2}); Counts/%.0f MeV/#it{c}^{2}", ptlimits[i], ptlimits[i + 1], prob_test[iprob].Data(), files[ifil].Data(), bin_width * 1000.);
+        h->SetTitle(histo_title.Data());
+        AliHFInvMassFitter* fitter = new AliHFInvMassFitter(h, min_fit, max_fit, background_fit_f, signal_fit_f);
+        fitter->SetUseLikelihoodFit();
+        fitter->SetInitialGaussianMean(mass);
+        if (fixsigma)
+          fitter->SetFixGaussianSigma(sigmafix[i]);
+        Bool_t out = fitter->MassFitter(0);
+        if (!out) {
+          fitter->GetHistoClone()->Draw();
+        }
+        fitter->DrawHere(gPad);
+        Double_t sigma = fitter->GetSigma();
+        fout->cd();
+        h->Write();
+
+        // subtracting background
+        TF1* bkgf = (TF1*)fitter->GetBackgroundFullRangeFunc();
+        TH1F* hbkg = (TH1F*)h->Clone("hbkg");
+        TH1F* hsigsub = (TH1F*)h->Clone("hsigsub");
+        for (int j = 0; j < h->GetNbinsX(); j++) {
+          float bkg = bkgf->Eval(hbkg->GetBinCenter(j + 1));
+          float bkge = TMath::Sqrt(bkg);
+          hbkg->SetBinContent(j + 1, bkg);
+          hbkg->SetBinError(j + 1, bkge);
+        }
+        //hbkg->Draw("same");
+        hsigsub->Add(hbkg, -1.);
+        c_fit_prob->cd(ifil + 4);
+        AliHFInvMassFitter* fitterbkg = new AliHFInvMassFitter(hsigsub, min_fit, max_fit, 3, signal_fit_f);
+        fitterbkg->SetUseChi2Fit();
+        fitterbkg->SetInitialGaussianMean(mass);
+        fitterbkg->SetFixGaussianSigma(sigma);
+        Bool_t out2 = fitterbkg->MassFitter(0);
+        if (!out2) {
+          fitterbkg->GetHistoClone()->Draw();
+        }
+        fitterbkg->DrawHere(gPad);
+      }
+      // merging 2 periods
+      hmerge->Rebin(rebin);
+      Float_t bin_width = hmerge->GetXaxis()->GetBinWidth(3);
+      TString histo_title = Form("%.0f-%.0f prob>%s; #it{M} (GeV/#it{c}^{2}); Counts/%.0f MeV/#it{c}^{2}", ptlimits[i], ptlimits[i + 1], prob_test[iprob].Data(), bin_width * 1000.);
+      hmerge->SetTitle(histo_title.Data());
+      c_fit->cd(iprob + 1);
+      gPad->SetTicks();
+      AliHFInvMassFitter* fitter = new AliHFInvMassFitter(hmerge, min_fit, max_fit, background_fit_f, signal_fit_f);
+      fitter->SetUseLikelihoodFit();
+      fitter->SetInitialGaussianMean(mass);
+      if (fixsigma)
+        fitter->SetFixGaussianSigma(sigmafix[i]);
+      Bool_t out = fitter->MassFitter(0);
+      if (!out) {
+        fitter->GetHistoClone()->Draw();
+      }
+      fitter->DrawHere(gPad);
+      c_fit_prob->cd(3);
+      fitter->DrawHere(gPad);
+      Double_t sigma = fitter->GetSigma();
+      double rawyield = fitter->GetRawYield();
+      double rawyielderr = fitter->GetRawYieldError();
+      h_raw_signal[iprob]->SetBinContent(i + 1, rawyield);
+      h_raw_signal[iprob]->SetBinError(i + 1, rawyielderr);
+
+      //h_signal
+
+      fout->cd();
+      hmerge->Write();
+
+      // subtracting background
+      TF1* bkgf = (TF1*)fitter->GetBackgroundFullRangeFunc();
+      TH1F* hbkg = (TH1F*)hmerge->Clone("hbkg");
+      TH1F* hsigsub = (TH1F*)hmerge->Clone("hsigsub");
+      for (int j = 0; j < hmerge->GetNbinsX(); j++) {
+        float bkg = bkgf->Eval(hbkg->GetBinCenter(j + 1));
+        float bkge = TMath::Sqrt(bkg);
+        hbkg->SetBinContent(j + 1, bkg);
+        hbkg->SetBinError(j + 1, bkge);
+      }
+      hsigsub->Add(hbkg, -1.);
+      c_fit_prob->cd(6);
+      AliHFInvMassFitter* fitterbkg = new AliHFInvMassFitter(hsigsub, min_fit, max_fit, 3, signal_fit_f);
+      //fitterbkg->SetUseLikelihoodFit();
+      fitterbkg->SetUseChi2Fit();
+      fitterbkg->SetInitialGaussianMean(mass);
+      fitterbkg->SetFixGaussianSigma(sigma);
+      Bool_t out2 = fitterbkg->MassFitter(0);
+      if (!out2) {
+        fitterbkg->GetHistoClone()->Draw();
+      }
+      fitterbkg->DrawHere(gPad);
+      c_fit->cd(iprob + 4);
+      fitterbkg->DrawHere(gPad);
+      c_fit_prob->Write();
+    }
+  }
+  fout->cd();
+  for (int k = 0; k < nprob_test; k++) {
+    h_raw_signal[k]->Write();
+  }
+}
